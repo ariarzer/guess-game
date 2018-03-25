d99a5a0f93c23684be200614bb7cd60b727df633
Ariarzer <ariarzer@gmail.com>
2018-03-25 18:58:17 +0300

    Vue is not defined

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,5 @@
+const Vue = require('vue');
+
 require('./components/registr');
 require('./components/results');
 require('./components/answer/answer-form');
@@ -6,26 +8,6 @@ var question = require('../../data/question.json');
 
 new Vue({
   el: '#guess-game',
-  template: `
-    <registration-form
-        v-if=render[0]
-        v-bind:score=score
-        v-on:i-finish='renderStep(0)'
-      >
-    </registration-form>
-    <answer-form
-        v-if=render[1]
-        v-bind:score=score
-        v-bind:answer=answer
-        v-on:i-finish='renderStep(1)'
-      >
-    </answer-form>
-    <showing-results
-        v-if=render[2]
-        v-bind:score=score
-        v-bind:answer=answer
-      >
-    </showing-results>`,
   data: {
     render: [true, false, false],
     score: [{id: 0, name: '', report: []}],
