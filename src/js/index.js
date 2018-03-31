2646002b21d189fff617e76caf6234183ae33523
Ariarzer <ariarzer@gmail.com>
2018-03-31 14:34:48 +0300

    Main template is removed from index.html to index.js

--- src/js/index.js
+++ src/js/index.js
@@ -8,10 +8,30 @@ import question from '../../data/question.json';
 
 new Vue({
   el: '#guess-game',
+  template: `
+    <registration-form
+      v-if=render[0]
+      v-bind:score=score
+      v-on:i-finish='renderStep(0)'
+      >
+    </registration-form>
+    <answer-form
+      v-if=render[1]
+      v-bind:score=score
+      v-bind:answer=answer
+      v-on:i-finish='renderStep(1)'
+      >
+    </answer-form>
+    <showing-results
+      v-if=render[2]
+      v-bind:score=score
+      v-bind:answer=answer
+      >
+    </showing-results>`,
   components: {
     'registration-form': registr,
-    'showing-results': results,
     'answer-form': answerForm,
+    'showing-results': results,
   },
   data: {
     render: [true, false, false],
